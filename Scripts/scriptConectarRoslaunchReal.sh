91290077a1f17bbd6ea1b898a685ec2ee3ac5c76
MatheusGodoi <31485083+MatheusGodoi@users.noreply.github.com>
2019-09-27 00:23:41 -0300

    Atualização scripts
    Atualização dos scripts com novos nomes da arquitetura

--- Scripts/scriptConectarRoslaunchReal.sh
+++ Scripts/scriptConectarRoslaunchReal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /home/claudio/drone_ws/devel/setup.bash
-gnome-terminal -- /opt/ros/melodic/bin/roslaunch mavros px4.launch fcu_url:=/dev/$1:57600
-#ttyUSB0
+gnome-terminal -- /opt/ros/melodic/bin/roslaunch mavros px4.launch fcu_url:=$1:57600
+#/dev/ttyUSB0
 
 #Matheus Vinicius G de Godoi
--- Scripts/scriptIniciaMissao.sh
+++ Scripts/scriptIniciaMissao.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /home/claudio/drone_ws/devel/setup.bash
-gnome-terminal -- /opt/ros/melodic/bin/rosrun drone_system init_Mission
+gnome-terminal -- /opt/ros/melodic/bin/rosrun decision_support init_Mission
 
 #Matheus Vinicius G de Godoi
